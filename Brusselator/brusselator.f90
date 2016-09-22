6e63ff952127abbf01768952736094b892f1658f
Benson Muite <bkmgit@users.noreply.github.com>
2016-09-22 17:20:14 +0300

    Update brusselator.f90

--- Brusselator/brusselator.f90
+++ Brusselator/brusselator.f90
@@ -5,7 +5,7 @@
 	!
 	! This program solves Brusselator equations in 3 dimensions
 	! u_t = a + u^2v - (b+1)u + Du (u_{xx} + u_{yy} + u_{zz})
-  ! v_t = bu - u^2v + Dv (v_{xx} + v_{yy} + v_{zz})
+        ! v_t = bu - u^2v + Dv (v_{xx} + v_{yy} + v_{zz})
 	!
 	! .. Parameters ..
 	!  Nx				= number of modes in x - power of 2 for FFT
@@ -87,30 +87,30 @@
 	! Declare variables
 	IMPLICIT NONE	
 	INCLUDE 'mpif.h'	
-	INTEGER(kind=4), PARAMETER	::  Nx=64
-	INTEGER(kind=4), PARAMETER 	::  Ny=64
-	INTEGER(kind=4), PARAMETER 	::  Nz=64
-	INTEGER(kind=4), PARAMETER ::  nmax=10000000
+	INTEGER(kind=4), PARAMETER	 ::  Nx=64
+	INTEGER(kind=4), PARAMETER 	 ::  Ny=64
+	INTEGER(kind=4), PARAMETER 	 ::  Nz=64
+	INTEGER(kind=4), PARAMETER       ::  nmax=10000000
 	REAL(kind=8), PARAMETER		 ::  Tmax=60.0
 	REAL(kind=8), PARAMETER		 ::  plotgap=0.05
-	REAL(kind=8), PARAMETER	   ::&
+	REAL(kind=8), PARAMETER	         ::&
 		pi=3.14159265358979323846264338327950288419716939937510d0
 	REAL(kind=8), PARAMETER		 ::  Lx=1.0,Ly=1.0,Lz=1.0
 	!equation specific
 	REAL(kind=8), PARAMETER		 ::  a=2.0d0
-	REAL(kind=8), PARAMETER    ::  b=18.20d0
+	REAL(kind=8), PARAMETER          ::  b=18.20d0
 	REAL(kind=8), PARAMETER		 ::  Du=0.050d0
 	REAL(kind=8), PARAMETER		 ::  Dv=0.050d0
-	REAL(kind=8)		           ::  dt=0.1**3
-	REAL(kind=8)	             ::  plottime=0.0
-	INTEGER(kind=4) 	        ::  plotnum=0
-	COMPLEX(kind=8)		::   utemp, vtemp, uhatemp
+	REAL(kind=8)		         ::  dt=0.1**3
+	REAL(kind=8)	                 ::  plottime=0.0
+	INTEGER(kind=4) 	         ::  plotnum=0
+	COMPLEX(kind=8)		         ::  utemp, vtemp, uhatemp
 	COMPLEX(kind=8), 	DIMENSION(:), ALLOCATABLE	::  kx,ky,kz
 	REAL(kind=8),  	        DIMENSION(:), ALLOCATABLE	::  x,y,z 
 	COMPLEX(kind=8), 	DIMENSION(:,:,:), ALLOCATABLE	::  uhigh,vhigh
 	REAL(kind=8),       DIMENSION(:,:,:), ALLOCATABLE	::  savefield
-	COMPLEX(kind=8), DIMENSION(:,:,:), ALLOCATABLE	::  uhat,vhat
-	REAL(kind=8), 	 DIMENSION(:), ALLOCATABLE	::  time
+	COMPLEX(kind=8), DIMENSION(:,:,:), ALLOCATABLE	        ::  uhat,vhat
+	REAL(kind=8), 	 DIMENSION(:), ALLOCATABLE	        ::  time
 	REAL(kind=8) 		::  modescalereal, myerr,allerr,mymaxv,mymaxu,maxu,maxv
 	INTEGER(kind=4)		::  i,j,k,l,n,m,mm,ll,AllocateStatus,stat
 	INTEGER(kind=4)		::  myid,numprocs,ierr
@@ -147,7 +147,7 @@
 	CHARACTER*100		::  name
 	name='./data/brusselator'
 
-    plottime=plotgap
+        plottime=plotgap
 	    ! initialisation of MPI
 	CALL system_clock(starttot,count_rate)	
 	CALL MPI_INIT(ierr)
@@ -159,7 +159,6 @@
 	CALL decomp_info_init(Nx,Ny,Nz,decomp) ! physical domain
 	! initialise FFT library
 	CALL decomp_2d_fft_init
-	
 
 	ALLOCATE(Uhigh(decomp%xst(1):decomp%xen(1),&
    				   decomp%xst(2):decomp%xen(2),&
@@ -201,12 +200,12 @@
 
 	!initial condition
 	CALL MPI_BARRIER(MPI_COMM_WORLD,ierr)
-  CALL initialdata(Nx,Ny,Nz,x,y,z,uhigh,vhigh,decomp)
+        CALL initialdata(Nx,Ny,Nz,x,y,z,uhigh,vhigh,decomp)
 	n=1
 	time(n)=0.0
 	plotnum=10000000
 	!Write initial data to disc
-    CALL savedata(Nx,Ny,Nz,plotnum,name,savefield,uhigh,vhigh,decomp)
+        CALL savedata(Nx,Ny,Nz,plotnum,name,savefield,uhigh,vhigh,decomp)
 	IF (myid.eq.0) THEN
 	 PRINT *,'Got initial data, starting timestepping'
 	END IF
@@ -215,15 +214,15 @@
 	DO WHILE ((n<nmax))
 	 n=n+1
      DO l=1,ss
-     ! Solve first nonlinear part
+      ! Solve first nonlinear part
       CALL nonlinear1(dt,aa(l),uhigh,vhigh,decomp)
       ! Solve second nonlinear part
       CALL nonlinear2(dt,bb(l),uhigh,vhigh,decomp)
-	  ! solve linear part exactly in Fourier space
-	  CALL linear(a,b,Du,Dv,dt,modescalereal,cc(l),kx,ky,kz,uhigh,vhigh,uhat,vhat,decomp)
-	 !end loop on stages for splitting method indexed by l
-	 END DO 		
-	 time(n)=time(n-1)+dt
+      ! solve linear part exactly in Fourier space
+      CALL linear(a,b,Du,Dv,dt,modescalereal,cc(l),kx,ky,kz,uhigh,vhigh,uhat,vhat,decomp)
+      !end loop on stages for splitting method indexed by l
+     END DO 		
+     time(n)=time(n-1)+dt
 
   !write data		
 	  IF (time(n).ge.plottime) THEN
@@ -242,15 +241,13 @@
 	IF ((myid.eq.0).and.(n.eq.nmax)) THEN
 	 PRINT *,'time',time(n),'n',n,'plot',plotnum
 	END IF
-    CALL savedata(Nx,Ny,Nz,plotnum,name,savefield,uhigh,vhigh,decomp)
+        CALL savedata(Nx,Ny,Nz,plotnum,name,savefield,uhigh,vhigh,decomp)
   	CALL system_clock(finish,count_rate)
 	IF (myid.eq.0) THEN
 	 PRINT *,'Finished time stepping steps:', n
 	 PRINT*,'Program took ',REAL(finish-start)/REAL(count_rate),'for time stepping', &
 	 REAL(finish-start)/REAL(count_rate)*numprocs
-     CALL saveresults(n,nmax,time,error,tries,name)
-	 PRINT *,'Saved data'
-	END IF
+     	END IF
 	CALL MPI_BARRIER(MPI_COMM_WORLD,ierr)
 	 ! clean up 
   	CALL decomp_2d_fft_finalize
@@ -258,7 +255,7 @@
  	DEALLOCATE(uhigh,vhigh,&
 		        uhat,vhat,&
    		        kx,ky,x,y,savefield,&	
-   		        time,error,tries,stat=AllocateStatus)	
+   		        time,stat=AllocateStatus)	
 	IF (AllocateStatus .ne. 0) STOP 
 
 	IF (myid.eq.0) THEN
